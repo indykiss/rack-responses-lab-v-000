6574ae5490495e0058a6b1d303ec6eae78313d97
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-12-21 21:10:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,11 +2,9 @@ class App
  
   def call(env)
     resp = Rack::Response.new
+    
+    if Time.now 
  
-    num_1 = Kernel.rand(1..11)
-    num_2 = Kernel.rand(12..24)
-
-    if num_1==num_2 && num_2==num_3
       resp.write "Good morning"
     else
       resp.write "Good afternoon"
