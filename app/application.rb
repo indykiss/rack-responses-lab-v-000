095e381aae12db26e04349a8be6e07f524158e13
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-12-21 21:10:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,7 @@ class App
   def call(env)
     resp = Rack::Response.new
     
-    if Time.now 
- 
+    if Time.now.hour.between?(0,11)
       resp.write "Good morning"
     else
       resp.write "Good afternoon"
