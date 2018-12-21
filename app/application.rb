5716f6399014404b29956d44cbe9807121250782
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-12-21 21:03:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,9 @@ class App
   def call(env)
     resp = Rack::Response.new
  
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
- 
+    num_1 = Kernel.rand(1..24)
+    num_2 = Kernel.rand(12..24)
+
     if num_1==num_2 && num_2==num_3
       resp.write "You Win"
     else
